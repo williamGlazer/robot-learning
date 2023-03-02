714f9bf1886a6ff3d08c98d113daf9344ffbc296
williamglazer <willcavanagh@hotmail.com>
2023-03-02 11:10:52 -0500

    added cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparaison of famous RL algorithms in JAX and other DL frameworks',
+    author='WilliamGlazer & SamuelAguilar',
+    license='MIT',
+)
